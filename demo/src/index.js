1ced3af114573e5f03e54f1c1a1133ec09e6f640
keyflight <olivier@andev.nl>
2019-04-28 13:00:43 +0700

    Initial commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,14 +1,71 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import {injectGlobal} from 'emotion'
+import {Canvas, Heading, Paragraph, Box} from '@cmds/demo-utils'
 
-import Example from '../../src'
+import {Dialog, DialogActions, DialogAction, DialogBody, DialogHeading} from '../../src'
+
+injectGlobal`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+        margin: 0;
+    }
+`
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>dialog Demo</h1>
-      <Example/>
-    </div>
+    return (
+        <Canvas>
+          <Heading>
+            Confirmation Dialog
+          </Heading>
+          <Paragraph>
+            With a heading, body, and two actions
+          </Paragraph>
+          <Box>
+            <Dialog>
+              <DialogHeading>
+                Are you sure you want to delete this table?
+              </DialogHeading>
+              <DialogBody>
+                All the data in this table will be lost.
+              </DialogBody>
+              <DialogActions>
+                <DialogAction>
+                  Cancel
+                </DialogAction>
+                <DialogAction danger>
+                  Delete
+                </DialogAction>
+              </DialogActions>
+            </Dialog>
+          </Box>
+          <Heading>
+            Alert Dialog
+          </Heading>
+          <Paragraph>
+            With a heading, body, and one action
+          </Paragraph>
+          <Box>
+            <Dialog>
+              <DialogHeading>
+                Are you sure you want to delete this table?
+              </DialogHeading>
+              <DialogBody>
+                All the data in this table will be lost.
+              </DialogBody>
+              <DialogActions>
+                <DialogAction primary>
+                  OK
+                </DialogAction>
+              </DialogActions>
+            </Dialog>
+          </Box>
+        </Canvas>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,92 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {css} from 'emotion'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
+export const DialogAction = ({primary, danger, onClick, children}) => (
+    <button
+        onClick={onClick}
+        className={css`
+            -moz-user-select: none;
+            -ms-flex: 1 1;
+            -ms-user-select: none;
+            -webkit-box-flex: 1;
+            -webkit-user-select: none;
+            background: #fff;
+            border: 0;
+            flex: 1 1;
+            font-weight: 700;
+            line-height: 50px;
+            padding: 0 10px;
+            user-select: none;
+            white-space: nowrap;
+            cursor: pointer;
+            text-align: center;
+            font-size: 16px;
+            border-bottom-left-radius: 6px;
+            border-bottom-right-radius: 6px;
+            &:nth-child(2) {
+                border-bottom-left-radius: 0;
+                border-left: 1px solid #d9d9d9;
+            }
+            &:focus {
+                outline: none;
+            }
+            &:hover {
+                background-color: #f9f9f9;
+            }
+            ${primary ? 'color: #07f;' : ''}
+            ${danger ? 'color: #f82b60;' : ''}
+        `}
+    >
+      {children}
+    </button>
+)
+
+export const DialogHeading = ({children}) => (
+    <div
+        className={css`
+            font-size: 19px;
+            font-weight: 700;
+            padding: 32px 32px 0 32px;
+        `}
+    >
+      {children}
     </div>
-  }
-}
+)
+
+export const DialogBody = ({children}) => (
+    <div
+        className={css`
+            padding: 32px;
+        `}
+    >
+      {children}
+    </div>
+)
+
+export const DialogActions = ({children}) => (
+
+    <div
+        className={css`
+            border-top: 1px solid #d9d9d9;
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+        `}
+    >
+      {children}
+    </div>
+)
+
+export const Dialog = ({children}) => (
+    <div
+        className={css`
+            background: #fff;
+            border-radius: 6px;
+            max-width: 360px;
+            margin: 0 auto;
+            border: 1px solid #d9d9d9;
+        `}
+    >
+      {children}
+    </div>
+)
